8103bc16e61407cfc228a0759f35f22f5445aeb0
PePorMAty <themranti3256@gmail.com>
2023-01-20 17:16:15 +0700

    work at home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { Clothes } from "./pages/Clothes/Clothes";
+import { Routes, Route, Link } from "react-router-dom";
+import { NewProducts } from "./pages/NewProducts/NewProducts";
+import { Collections } from "./pages/Collections/Collections";
+import { Home } from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/newproducts" element={<NewProducts />} />
+        <Route path="/collections" element={<Collections />} />
+        <Route path="/clothes" element={<Clothes />} />
+      </Routes>
     </div>
   );
 }
